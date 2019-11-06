7777f73e322740685c7147447c1e2d644d7f808f
Pat Hickey <phickey@fastly.com>
2019-11-06 14:55:54 -0800

    tests: try to narrow failure a bit

--- tools/witx/tests/wasi_unstable.rs
+++ tools/witx/tests/wasi_unstable.rs
@@ -31,10 +31,30 @@ fn render_roundtrip() {
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 
     let back_to_sexprs = format!("{}", doc);
-    println!("{}", back_to_sexprs);
+
     let doc2 = witx::parse(&back_to_sexprs)
         .map_err(|e| e.report_with(&witx::MockFs::new(&[("-", &back_to_sexprs)])))
         .unwrap();
 
-    assert_eq!(doc, doc2);
+    // I'd just assert_eq, but when it fails the debug print is thousands of lines long and impossible
+    // to figure out where they are unequal.
+    if doc != doc2 {
+        for type_ in doc.datatypes() {
+            let type2 = doc2.datatype(&type_.name).expect("doc2 missing datatype");
+            assert_eq!(type_, type2);
+        }
+        for mod_ in doc.modules() {
+            let mod2 = doc2.module(&mod_.name).expect("doc2 missing module");
+            for import in mod_.imports() {
+                let import2 = mod2.import(&import.name).expect("mod2 missing import");
+                assert_eq!(import, import2);
+            }
+            for func in mod_.funcs() {
+                let func2 = mod2.func(&func.name).expect("mod2 missing func");
+                assert_eq!(func, func2);
+            }
+        }
+    }
+    // This should be equivelant to the above, but just in case some code changes where it isnt:
+    assert_eq!(doc, doc2)
 }
