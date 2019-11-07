0051e50ddf96a4c8c5a81ef6956630e4d8cd93c3
Pat Hickey <phickey@fastly.com>
2019-11-07 13:47:02 -0800

    witx: add support for parsing multiple top-level witx files

--- tools/witx/tests/wasi_unstable.rs
+++ tools/witx/tests/wasi_unstable.rs
@@ -3,31 +3,31 @@ use witx;
 
 #[test]
 fn validate_wasi_unstable_preview0() {
-    witx::load(Path::new(
+    witx::load(&[Path::new(
         "../../phases/unstable/witx/wasi_unstable_preview0.witx",
-    ))
+    )])
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 }
 
 #[test]
 fn validate_wasi_ephemeral_preview() {
-    witx::load(Path::new(
+    witx::load(&[Path::new(
         "../../phases/ephemeral/witx/wasi_ephemeral_preview.witx",
-    ))
+    )])
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 }
 
 #[test]
 fn validate_wasi_old_preview0() {
-    witx::load(Path::new("../../phases/old/witx/wasi_unstable.witx"))
+    witx::load(&[Path::new("../../phases/old/witx/wasi_unstable.witx")])
         .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 }
 
 #[test]
 fn render_roundtrip() {
-    let doc = witx::load(Path::new(
+    let doc = witx::load(&[Path::new(
         "../../phases/unstable/witx/wasi_unstable_preview0.witx",
-    ))
+    )])
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 
     let back_to_sexprs = format!("{}", doc);
